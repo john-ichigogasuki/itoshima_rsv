09f8a51dd4040370d47f3416ec0165028d00ca40
koga yudai <john@JohnMacBookPro.local>
2024-07-18 22:58:46 +0900

    第一段階で普通にログインできない，Cookieは取得できていない，Headerかdataに問題があるので調べる

--- reservation.py
+++ reservation.py
@@ -7,31 +7,37 @@ session = requests.Session()
 
 # 最初のログイン遷移
 url_signin = "https://www.task-asp.net/cu/ykd402303/Top"
+
+#sign inに必要なsessionの取得
 response = session.get(url_signin)
+target_cookie = response.cookies.get_dict().get('taskasp_cu_ykd_sessionid', None)
+print(f'target_cookie: {target_cookie}')
 
 headers = {
-    "Connection": "keep-alive",
-    "Content-Length": "22984",
-    "Cache-Control": "max-age=0",
+    # "Connection": "keep-alive",
+    # "Cache-Control": "max-age=0",
     "Origin": "https://www.task-asp.net",
-    "Upgrade-Insecure-Requests": "1",
+    # "Upgrade-Insecure-Requests": "1",
     "Content-Type": "application/x-www-form-urlencoded",
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-User": "?1",
-    "Sec-Fetch-Dest": "document",
-    "Referer": "https://www.task-asp.net/cu/ykd402303/Top",
+    # "Sec-Fetch-Site": "same-origin",
+    # "Sec-Fetch-Mode": "navigate",
+    # "Sec-Fetch-User": "?1",
+    # "Sec-Fetch-Dest": "document",
+    # "Referer": "https://www.task-asp.net/cu/ykd402303/Top",
     "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "ja",
-    # "Cookie": "taskasp_cu_ykd_starturl=%2fcu%2feg%2fykd402303.task; taskasp_cu_ykd_sppage=1; taskasp_cu_ykd_sessionid=adufsolgpg2o1rz3d4ug5tsn",
-    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
-    "sec-ch-ua-mobile": "?1",
-    "sec-ch-ua-platform": "Android",
-    "Sec-GPC": "1",
+    "Cookie": "taskasp_cu_ykd_starturl=%2fcu%2feg%2fykd402303.task; taskasp_cu_ykd_sppage=1; taskasp_cu_ykd_sessionid="+str(target_cookie),
+    # "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+    # "sec-ch-ua-mobile": "?1",
+    # "sec-ch-ua-platform": "macOS",
+    # "Sec-GPC": "1",
+    "Host": "www.tast-asp.net",
+    "Referer": "https://www.task-asp.net/cu/ykd402303/Top",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
 }
 
+print(headers)
 load_dotenv()
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
