ad4dc36d0e8bdbefd637f6164e560830e2de5387
“john-ichigogasuki” <yudai.k.1093@gmail.com>
2024-09-12 00:12:43 +0900

    a

--- reservationBySelenium.py
+++ reservationBySelenium.py
@@ -1,4 +1,24 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
+def getSignInUrl():
+    options = Options()
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')
+    driver.get("https://www.task-asp.net/cu/eg/ykd402303.task")
+
+def executeLogIn():
+    logInButton = driver.find_element(By.ID, "UserLoginLinkButton")
+    logInButton.click()
+    sleep(0.2)
+    
+    ID_area = driver.find_element(By.ID, "UserNoTextBox")
+    PW_area = driver.find_element(By.ID, "PasswordTextBox")
+    ID_area.send_keys(os.getenv('USERNAME'))
+    PW_area.send_keys(os.getenv('PASSWORD'))
+
+getSignInUrl()
+executeLogIn()
